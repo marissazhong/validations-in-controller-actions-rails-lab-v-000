4622014bcff700fef362d45ddc080e1f3bd79ae0
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 15:55:07 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,10 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    @post = Post.find(post_params)
+
+    if @post.valid?
+      @post.update(post_params)
 
     redirect_to post_path(@post)
   end
